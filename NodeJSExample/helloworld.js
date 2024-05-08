8f9f22d3a9744c296befb93c36ff71a0036dfa0e
realtimestert <istewpotter1@gmail.com>
2024-05-07 21:06:54 -0700

    initial commit

--- NodeJSExample/helloworld.js
+++ NodeJSExample/helloworld.js
@@ -2,4 +2,11 @@ const name = "Stuart"
 
 console.log("Hello world")
 
-//pull
+console.log("bye")
+
+//> git push origin main:main
+//remote: error: GH006: Protected branch update failed for refs/heads/main.
+//remote: error: Changes must be made through a pull request.
+//To github.com:realtimestert/itm350-realtimestert-test.git
+// ! [remote rejected] main -> main (protected branch hook declined)
+//error: failed to push some refs to 'github.com:realtimestert/itm350-realtimestert-test.git'
